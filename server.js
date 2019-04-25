2dccba4d5e721f8d12a087b3c6714849027f0923
Paul Shivers <paulsshivers@gmail.com>
2019-04-25 13:22:00 -0400

    changed server port

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/location', (req, res) => {
 });
 
 //ALWAYS AT BOTTOM OF FILE
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(process.env.PORT, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
