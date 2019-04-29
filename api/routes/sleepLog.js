049a532128d213fec00bc05c2d703bdfbfd9e6a3
Paul Shivers <paulsshivers@gmail.com>
2019-04-29 14:51:31 -0400

    added post and patch views for nights

--- api/routes/sleepLog.js
+++ api/routes/sleepLog.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('nights');
-})
-
-router.get('/:nightId', (req, res) => {
-  res.render("nightGet");
+  res.render('sleepLog/nightsRead');
 });
+
+router.get('/nightPost', (req, res, next) => {
+  res.render('sleepLog/nightPost');
+});
+
+router.get('/nightPatch', (req, res, next) => {
+  res.render('sleepLog/nightPatch');
+});
+// router.get('/:nightId', (req, res) => {
+//   res.render("");
+// });
 
 module.exports = router;
